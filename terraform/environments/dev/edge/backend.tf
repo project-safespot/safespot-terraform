f865060bece1fb06afe18072c763c7e228656dc9
wo969776-cloud <wo969776@gmail.com>
2026-04-16 15:59:34 +0900

    chore: remove dynamodb lock from backend config

--- terraform/environments/dev/edge/backend.tf
+++ terraform/environments/dev/edge/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "safespot-terraform-state"
     key            = "environments/dev/edge/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "safespot-tf-lock"
     encrypt        = true
   }
 }
--- terraform/environments/dev/network/backend.tf
+++ terraform/environments/dev/network/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "safespot-terraform-state"
     key            = "environments/dev/network/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "safespot-tf-lock"
     encrypt        = true
   }
 }
