b4a9a17105182889cbb3ebcd5426fa34107d327e
naladoodong <naladoodong@gmail.com>
2026-04-21 10:45:26 +0900

    chore(terraform): add outputs for dev data

---
+++ terraform/environments/dev/data/outputs.tf
@@ -0,0 +1,34 @@
+output "aurora_cluster_endpoint" {
+  description = "Aurora writer endpoint"
+  value       = module.rds.cluster_endpoint
+}
+
+output "aurora_reader_endpoint" {
+  description = "Aurora reader endpoint"
+  value       = module.rds.cluster_reader_endpoint
+}
+
+output "aurora_port" {
+  description = "Aurora port"
+  value       = module.rds.cluster_port
+}
+
+output "aurora_db_name" {
+  description = "Database name"
+  value       = module.rds.db_name
+}
+
+output "redis_primary_endpoint" {
+  description = "Redis primary (writer) endpoint"
+  value       = module.redis.redis_primary_endpoint
+}
+
+output "redis_reader_endpoint" {
+  description = "Redis reader endpoint (레플리카 로드밸런싱)"
+  value       = module.redis.redis_reader_endpoint
+}
+
+output "redis_port" {
+  description = "Redis port"
+  value       = module.redis.redis_port
+}
