17f423f19042e8d90bbc5440481931f293498fa5
naladoodong <naladoodong@gmail.com>
2026-05-05 15:08:39 +0900

    feat: addon irsa 모듈 추가

--- terraform/environments/dev/api-service/eks-addons-irsa/backend.tf
+++ terraform/environments/dev/api-service/eks-addons-irsa/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "safespot-terraform-state"
-    key     = "environments/dev/api-service/eks-addons/terraform.tfstate"
+    key     = "environments/dev/api-service/eks-addons-irsa/terraform.tfstate"
     region  = "ap-northeast-2"
     encrypt = true
   }
