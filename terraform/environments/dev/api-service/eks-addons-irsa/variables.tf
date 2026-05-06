582c5a9fb9740ce7b5782d516decd1dafeb4603c
naladoodong <naladoodong@gmail.com>
2026-05-07 01:41:16 +0900

    chore: align ssm parameters with api-service base

---
+++ terraform/environments/dev/api-service/eks-addons-irsa/variables.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "remote_state_bucket" {
+  description = "S3 bucket name for Terraform remote state"
+  type        = string
+  default     = "safespot-terraform-state"
+}
+
+variable "eks_core_state_key" {
+  description = "S3 object key for EKS core Terraform state"
+  type        = string
+}
+
+variable "external_dns_namespace" {
+  description = "Kubernetes namespace for ExternalDNS"
+  type        = string
+  default     = "external-dns"
+}
+
+variable "external_dns_service_account_name" {
+  description = "Kubernetes service account name for ExternalDNS"
+  type        = string
+  default     = "external-dns"
+}
+
+variable "external_dns_zone_name" {
+  description = "Route53 public hosted zone name managed by ExternalDNS"
+  type        = string
+  default     = "safespot.site"
+}
+
+variable "external_secrets_namespace" {
+  description = "Kubernetes namespace for External Secrets Operator"
+  type        = string
+  default     = "external-secrets"
+}
+
+variable "external_secrets_service_account_name" {
+  description = "Kubernetes service account name for External Secrets Operator"
+  type        = string
+  default     = "external-secrets"
+}
---
+++ terraform/modules/api-service/argocd-bootstrap/argocd.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.argocd_chart_version
+  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
+
+  values = var.argocd_values != "" ? [var.argocd_values] : []
+
+  wait    = true
+  timeout = 600
+}
