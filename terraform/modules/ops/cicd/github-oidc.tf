239eb371efe3304f4dfe0c5de189ad1a651f3e45
2sso <201sohyun@naver.com>
2026-05-08 12:13:11 +0900

    fix: thumbprint_list

--- terraform/modules/ops/cicd/github-oidc.tf
+++ terraform/modules/ops/cicd/github-oidc.tf
@@ -7,7 +7,9 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com"
   ]
 
-  thumbprint_list = []
+  thumbprint_list = [
+  "7560d6f40fa55195f740ee2b1b7c0b4836cbe103"
+  ]
 
   tags = merge(var.common_tags, {
     Name    = "${local.name_prefix}-iam-oidc-github"
